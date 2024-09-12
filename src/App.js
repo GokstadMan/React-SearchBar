c422513bee9e28e35a979e4090c551a3254f5919
Benjo <ben.ohman@gokstadakademiet.no>
2024-09-12 08:19:16 +0200

    searchBar finished with 3 databases uploaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import SearchBar from './Components/SearchBar';
+// import BookData from "./Data.json";
+import CarData from "./Cars.json";
+// import MovieData from "./Movies.json";
+import "./Components/SearchBar.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React SearchBar</h1>
+      <SearchBar placeholder="Title..." data={CarData}/>
     </div>
   );
 }
